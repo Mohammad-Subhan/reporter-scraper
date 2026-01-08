007447d0e7b2c7f4b3f146285dc89501974582c1
Mohammad Subhan <157013912+Mohammad-Subhan@users.noreply.github.com>
2026-01-08 11:31:36 +0500

    Print TWITTER_API_KEY for debugging purposes

--- twitter.py
+++ twitter.py
@@ -11,6 +11,8 @@ ACCESS_TOKEN = os.environ.get("AIRTABLE_ACCESS_TOKEN")
 BASE_ID = os.environ.get("AIRTABLE_BASE_ID")
 TWITTER_API_KEY = os.environ.get("TWITTER_API_KEY")
 
+print(TWITTER_API_KEY)
+
 queries = [
     "periodista Puerto Rico",
     "reportero Puerto Rico",
